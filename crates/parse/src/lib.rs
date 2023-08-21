bc79df98a3c2094491c4e20ec5da0514024c5abc
sorry <247076126@qq.com>
2023-08-22 00:04:47 +0800

    t2

--- crates/parse/src/lib.rs
+++ crates/parse/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod types;
 
 use crate::types::{Declaration, Root, Rule, RuleOrAtRuleOrDecl};
@@ -8,25 +10,20 @@ type CurrentToken = TokenNode;
 struct Parser {
     tokenize: Tokenize,
     bucket: Vec<TokenNode>,
-    current_token: Option<TokenNode>,
 }
 
 impl Parser {
     fn new(input: String) -> Self {
         let tokenize = Tokenize::new(input);
         let bucket = Vec::new();
-        Self {
-            tokenize,
-            bucket,
-            current_token: None,
-        }
+        Self { tokenize, bucket }
     }
     fn parse_root(&mut self) -> Root {
         let mut children = Vec::new();
         while !self.tokenize.is_eof() {
-            let owed_token = self.tokenize.when(None);
+            self.tokenize.when(None);
 
-            if let Some(current_token) = &self.current_token {
+            if let Some(ref current_token) = self.tokenize.current_token {
                 let builder_node = match current_token.maybe_syntax() {
                     Some(Token) => match Token {
                         Token::OpenCurly => Some(RuleOrAtRuleOrDecl::Rule(self.parse_rule())),
@@ -65,8 +62,8 @@ impl Parser {
                 let builder_node = match CurrentToken.maybe_syntax() {
                     Some(Token) => match Token {
                         Token::OpenCurly => Some(RuleOrAtRuleOrDecl::Rule(self.parse_rule())),
+                        Token::COLON => self.prase_maybe_decl_or_rule(),
                         Token::AT => todo!(),
-                        Token::COLON => Some(RuleOrAtRuleOrDecl::Declaration(self.parse_decl())),
                         Token::CloseCurly => None,
                         Token::SEMICOLON => None,
                         _ => {
@@ -89,26 +86,57 @@ impl Parser {
 
     fn parse_decl(&mut self) -> Declaration {
         // 触碰到 `{` 符号 桶里面前面不应该是空的
-        if self.bucket.is_empty() {
+        if self.bucket.is_empty() || self.bucket.len() != 2 {
             panic!("syntax error")
         }
-        let key = self.bucket.last().unwrap().to_owned();
+        let key = self.bucket.get(0).unwrap().to_owned();
+        let value = self.bucket.get(0).unwrap().to_owned();
         self.bucket.clear();
-        let mut value: Option<TokenNode> = None;
-        let maybe_value = self.tokenize.next().unwrap();
-        if !maybe_value.is_syntax {
-            value = Some(maybe_value);
-        }
-        if value.is_none() {
-            panic!("syntax error");
-        }
-        Declaration::new(key, value.unwrap())
+        Declaration::new(key, value)
     }
 
     fn prase_maybe_decl_or_rule(&mut self) -> Option<RuleOrAtRuleOrDecl> {
         if self.bucket.len() > 0 {
-            return Some(RuleOrAtRuleOrDecl::Declaration(self.parse_decl()));
+            while !self.tokenize.is_eof() {
+                self.tokenize.when(None);
+                match self.tokenize.current_token.to_owned() {
+                    Some(CurrentToken) => {
+                        let builder_node = match CurrentToken.maybe_syntax() {
+                            Some(Token) => match Token {
+                                Token::OpenCurly => {
+                                    Some(RuleOrAtRuleOrDecl::Rule(self.parse_rule()))
+                                }
+                                Token::SEMICOLON => self.prase_maybe_decl_or_rule(),
+                                _ => {
+                                    self.bucket.push(CurrentToken);
+                                    None
+                                }
+                            },
+                            None => {
+                                self.bucket.push(CurrentToken);
+                                None
+                            }
+                        };
+                        if builder_node.is_some() {
+                            return builder_node;
+                        }
+                    }
+                    None => {}
+                }
+            }
+            panic!("syntax error")
         }
+        let colon_symbol = self.tokenize.current_token.to_owned().unwrap();
+        let next_selector = self.tokenize.next().unwrap();
+
+        self.bucket
+            .push(TokenNode::merge(colon_symbol, next_selector));
+
+        //下一个符号是应该是 `{`
+        if !self.tokenize.when(Some(Token::OpenCurly)) {
+            panic!("syntax error");
+        }
+
         return Some(RuleOrAtRuleOrDecl::Rule(self.parse_rule()));
     }
 }
@@ -148,12 +176,30 @@ mod test {
     fn parse_define() {
         let input = String::from(
             ":xx {
-                color:red;
+                --good:red;
             }",
         );
         let mut parser = Parser::new(input);
         let root = parser.parse_root();
 
-        format!("{:#?}", root);
+        let test_parse = fs::read_to_string("./src/__snapshots__/parse_define.snap").unwrap();
+        assert_eq!(format!("{:#?}", root), test_parse);
+        // fs::write(
+        //     "./src/__snapshots__/parse_define.snap",
+        //     format!("{:#?}", root),
+        // );
+    }
+    #[test]
+    fn prase_colon() {
+        let input = String::from(
+            "input:after{
+                color:red;
+            }",
+        );
+
+        let mut parser = Parser::new(input);
+        let root = parser.parse_root();
+
+        println!("{:#?}", root);
     }
 }
